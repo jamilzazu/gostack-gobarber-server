e5c97d875a26e23cc0be03c0998e727e51a2aa6c
Jamil Zazu <jamillzazu@gmail.com>
2020-04-23 22:27:43 -0400

    Criando DTO de agendamentos

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,6 +1,11 @@
 import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointments';
 
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
 class AppoitmentsRepository {
   private appointments: Appointment[];
 
@@ -8,8 +13,8 @@ class AppoitmentsRepository {
     this.appointments = [];
   }
 
-  public create(provider: string, date: Date): Appointment {
-    const appointment = new Appointment(provider, date);
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
 
     this.appointments.push(appointment);
 
